b4c940142bb58f7a3896b8cd34112fc23c703d1c
TheOkster <jokioluda@gmail.com>
2024-03-11 22:19:35 -0400

    Optimized some code

--- main.py
+++ main.py
@@ -8,91 +8,103 @@ import xml.etree.ElementTree as ET
 def truncate_titleid(titleid: str):
     """
     Truncates the titleid
-    :param titleid: the original TitleID
-    :return: The truncated TitleID
+    :param titleid: the original titleid
+    :return: The truncated titleid
     """
     return titleid[-8:].upper()
 
 
-def parse_xml():
+def parse_data():
     """
-    Parses
-    :return: Returns a ga
+    Parses the 3dsreleases.xml, DSi.csv, and eShop.csv
+    :return: Returns a games dictionary with internal dictionaries with keys as the region
     """
-    # create element tree object
     tree = ET.parse('3dsreleases.xml')
-
-    # get root element
     root = tree.getroot()
 
-    gamesitems = []
+    games = {}
 
-    # iterate news items
     for item in root.findall('./'):
-        games = {}
-
+        # Goes through the XML
+        game_details = {'platform': '3DS'}
+        title_id = ''
+        region = ''
         for child in item:
             if child.tag == 'titleid':
-                games["titleid"] = truncate_titleid(child.text)
+                title_id = truncate_titleid(child.text)
             if child.tag == 'name':
-                games["name"] = child.text
+                game_details["name"] = child.text
             if child.tag == 'region':
-                games["region"] = child.text
-            if child.tag in ('titleid', 'name', 'platform', 'region'):
-                games["platform"] = "3DS"
-        if games:
-            gamesitems.append(games)
+                region = child.text
+        if title_id:
+            if title_id in games:
+                games[title_id][region] = game_details
+            else:
+                games[title_id] = {region: game_details}
     with open('System.csv', mode='r') as f:
-        csvFile = csv.reader(f)
+        csv_file = csv.reader(f)
+        for lines in csv_file:
+            title_id = truncate_titleid(lines[1])
+            if title_id in games:
+                games[title_id][''] = {"name": lines[7], "platform": "3DS System"}
+            else:
+                games[title_id] = {'': {"name": lines[7], "platform": "3DS System"}}
 
-        for lines in csvFile:
-            games = {"name": lines[7], "titleid": truncate_titleid(lines[1]), "region": "", "platform": "System"}
-            gamesitems.append(games)
     with open('DSi.csv', mode='r', encoding='utf-8') as f:
-        csvFile = csv.reader(f)
-
-        for lines in csvFile:
-            games = {"name": lines[3], "titleid": truncate_titleid(lines[1]), "region": "", "platform": "DSi"}
-            gamesitems.append(games)
+        csv_file = csv.reader(f)
+        for lines in csv_file:
+            title_id = truncate_titleid(lines[1])
+            if title_id in games:
+                games[title_id][''] = {"name": lines[3], "platform": "DSi", "region": lines[4]}
+            else:
+                games[title_id] = {'': {"name": lines[3], "platform": "DSi", "region": lines[4]}}
     with open('eShop.csv', mode='r', encoding='utf-8') as f:
-        csvFile = csv.reader(f)
+        csv_file = csv.reader(f)
+        for lines in csv_file:
+            title_id = truncate_titleid(lines[1])
+            if lines[5] == 'Japan':
+                lines[5] = 'JPN'
+            if lines[5] == 'Europe/Australia':
+                lines[5] = 'EUR'
+            if lines[5] == 'China':
+                lines[5] = 'CHN'
+            if lines[5] == 'Korea':
+                lines[5] = 'KOR'
+            if title_id not in games:
+                games[title_id] = {'': {"name": lines[0], "platform": "3DS", "region": lines[5]}}
+            elif lines[5] not in games[title_id]:
+                games[title_id][lines[5]] = {"name": lines[0], "platform": "3DS", "region": lines[5]}
+    # with open('Manual.csv', mode='r', encoding='utf-8') as f:
+    #     csvFile = csv.reader(f)
+    #
+    #     for lines in csvFile:
+    #         if not any(dictionary['titleid'] == lines[1] for dictionary in gamesitems):
+    #             game = {"name": lines[1], "titleid": lines[0].upper(), "region": "", "platform": lines[2]}
+    #             gamesitems.append(game)
+    return games
 
-        for lines in csvFile:
-            if not any(dictionary['titleid'] == lines[1] for dictionary in gamesitems):
-                games = {"name": lines[0], "titleid": truncate_titleid(lines[1]), "region": lines[5], "platform": "3DS"}
-                gamesitems.append(games)
-    with open('Manual.csv', mode='r', encoding='utf-8') as f:
-        csvFile = csv.reader(f)
 
-        for lines in csvFile:
-            if not any(dictionary['titleid'] == lines[1] for dictionary in gamesitems):
-                games = {"name": lines[1], "titleid": lines[0].upper(), "region": "", "platform": lines[2]}
-                gamesitems.append(games)
-    return gamesitems
+releases = parse_data()
 
 
-releases = parse_xml('3dsreleases.xml')
-
-
-def match_name(titleid: str):
-    name = ""
-    for release in releases:
-        if release["titleid"].upper() == titleid.upper():
-            if release["region"] == "USA":
-                return release["name"]
-            name = release["name"]
-    return name
+#
+def match_name(titleid: str, region_priority=("USA", "EUR", "JPN", "KOR")):
+    titleid = titleid.upper()
+    if releases.get(titleid):
+        for region in region_priority:
+            if region in releases[titleid]:
+                return releases[titleid][region]["name"]
+        return next(iter(releases[titleid.upper()].values()))['name']
+    return "Unknown"
 
 
 def match_platform(titleid: str):
-    for release in releases:
-        truncated_titleid = release["titleid"][-8:]
-        if truncated_titleid == titleid.upper():
-            return release["platform"]
-    return ""
+    print(titleid.upper())
+    if titleid.upper() in releases:
+        return next(iter(releases[titleid.upper()].values()))['platform']
+    return "Unknown"
 
-
-games = [["Truncated TitleID", "Name", "Time", "Times Played"]]
+games_csv = [["Truncated TitleID", "Name", "Time", "Times Played"]]
 with open('pld.dat', 'rb') as f:
     file = f.read()
     file = file[0x000C3510:]
@@ -111,9 +123,8 @@ with open('pld.dat', 'rb') as f:
         reversed_array.reverse()
         titleid = ''.join(["{0:0{1}x}".format(x, 2) for x in reversed_array])
         entry = [titleid, match_name(titleid), match_platform(titleid), time_played, times_opened]
-        print(entry)
         if entry:
-            games.append(entry)
+            games_csv.append(entry)
 with open('3ds.csv', 'w', newline='') as f:
     write = csv.writer(f)
-    write.writerows(games)
+    write.writerows(games_csv)
